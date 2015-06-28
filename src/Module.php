d11f9172de566280b1a49161ebaad84da93b58ee
merlin <romain.dary@eoko.fr>
2015-06-28 19:09:37 +0200

    Update module structure

---
+++ src/Module.php
@@ -0,0 +1,21 @@
+<?php
+namespace Eoko\Mandrill;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/',
+                ],
+            ],
+        ];
+    }
+}
